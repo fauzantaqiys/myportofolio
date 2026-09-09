5479db323d0b40807125c2c5faf1b3abbd0be6a3
fauzantaqiys <fauzansantosa456@gmail.com>
2026-09-09 21:25:17 +0700

    fix: pin django 5.0 and restore standard whitenoise storage

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 
 # Email
